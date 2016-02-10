89808752b12cb3a5c10957b85cf5c249c75e2e4c
avoid3d <avoid3d@gmail.com>
2016-02-10 19:27:13 +0200

    Added html boilerplate.

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
 
+gulp.task('copy-index', function() {
+  gulp.src('./index.html')
+ .pipe(gulp.dest('./build'));
+})
+
+gulp.task('copy-images', function() {
+  gulp.src('./images/favicon.ico')
+ .pipe(gulp.dest('./build'));
+})
+
 // Default task
-gulp.task('default', function() {
-  console.log('TODO: Build the project.');
-});
+gulp.task('default', ['copy-index', 'copy-images']);
