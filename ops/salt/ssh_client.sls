22482445bc2649d04ea562a29a79c55557309664
avoid3d <avoid3d@gmail.com>
2016-02-15 23:19:53 +0200

    Added turnip kurbenetes config.

--- ops/salt/ssh_client.sls
+++
@@ -1,5 +0,0 @@
-ssh_client:
-  ssh_auth.present:
-    - user: waverbase
-    - enc: ssh-rsa
-    - name: {{pillar['saltmaster_ssh_public_key']}}
---
+++ ops/turnip/build.sh
@@ -0,0 +1,2 @@
+docker build -t eu.gcr.io/api-project-1075799951054/prod-turnip -f Dockerfile .
+gcloud docker push eu.gcr.io/api-project-1075799951054/prod-turnip
