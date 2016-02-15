d668d3d3bbed77896af1da667c31a90b7f5161c1
avoid3d <avoid3d@gmail.com>
2016-02-15 23:28:35 +0200

    Added turnip to circleci build.

--- ops/turnip/build.sh
+++ ops/turnip/build.sh
@@ -1,2 +1,1 @@
 docker build -t eu.gcr.io/api-project-1075799951054/prod-turnip -f Dockerfile .
-gcloud docker push eu.gcr.io/api-project-1075799951054/prod-turnip
---
+++ ops/turnip/push.sh
@@ -0,0 +1,1 @@
+gcloud docker push eu.gcr.io/api-project-1075799951054/prod-turnip
