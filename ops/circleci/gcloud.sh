d3205c94efeb1a9305ba1d86c653cd35292cec41
avoid3d <avoid3d@gmail.com>
2016-02-16 00:46:37 +0200

    Removed debug logs.

--- ops/circleci/gcloud.sh
+++ ops/circleci/gcloud.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 set -e
-echo $GCLOUD_KEY
-echo $GCLOUD_EMAIL
 echo $GCLOUD_KEY | base64 --decode > gcloud.json
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.json
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
