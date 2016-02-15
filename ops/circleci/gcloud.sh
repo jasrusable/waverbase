ed872b0474c5e1d21f0519bf91486f69011648b6
avoid3d <avoid3d@gmail.com>
2016-02-16 00:33:40 +0200

    Skipped updating gcloud cli.

--- ops/circleci/gcloud.sh
+++ ops/circleci/gcloud.sh
@@ -5,6 +5,7 @@ echo $GCLOUD_EMAIL
 echo $GCLOUD_KEY | base64 --decode > gcloud.json
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.json
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
-gcloud --quiet components update
+#TODO: Figure out how to update this.
+#gcloud --quiet components update
 gcloud config set project $GCLOUD_PROJECTID
 gcloud docker --authorize-only
