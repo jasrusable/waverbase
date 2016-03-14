2dc0f20c0a7d2902a007cbb220763cc1c6b749bd
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2016-03-14 13:00:14 +0200

    mongo: better mongo docker service

--- mongo/service/service.js
+++ mongo/service/service.js
@@ -14,43 +14,6 @@ var gcompute = gcloud
 
 var server = thrift.createServer(MongoService, {
     createServer(appName, diskSize, ramSize, result) {
-	// create the disk first
-	var disk = gcompute.disk(appName+'-mongo-disk');
-	// since app name is unique if the disk exists that means that we have previously created the mongo server
-	console.log('stuff');
-	var deferred = Q.defer();
-	/*disk.exists(function(err, exists) {
-	    if (err) {
-		deferred.reject(new Error(err));
-	    } else {
-		deferred.resolve(exists);
-	    }
-	});*/
-	//deferred.promise
-
-	Q.nfcall((callback) => disk.exists(callback))
-	    .then((exists) => {
-		if (exists) {
-		    console.log('it exists');
-		    return;
-		}
-		console.log('it does not exist');
-		return Q.nfcall(gcompute.create,
-		{
-		    sizeGb: diskSize,
-		    name: appName+'-mongo-disk',
-		    description: 'Disk used by mongodb for app '+appName
-		})
-	    })
-	    .then(function(err, disk, operation, apiResponse) {
-		if(err) {
-		    result(false, 'gcloud failed on creating disk '+err);
-		    return;
-		}
-		console.log(disk);
-		console.log(operation);
-		console.log(apiResponse);
-	    });
     },
     getServer(appName) {
 	return 'hello world';
