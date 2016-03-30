e048c29bf3021b211e05c4ba52661f2ce366dc30
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2016-03-30 15:25:33 +0200

    sidecar: remove old sidecar

--- mongo/sidecar/src/harness.js
+++
@@ -1,9 +0,0 @@
-var mongo = require('./lib/mongo');
-
-mongo.getDb(function(err, db) {
-    console.log('got db');
-    mongo.initReplSet(db, '127.0.0.1:27017', function(err, result) {
-	console.log('everything completed ');
-	console.log(err);
-    });
-});
