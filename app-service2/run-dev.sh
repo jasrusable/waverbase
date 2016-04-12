c43df189d2e0a9ba0b83c9aaa5b560d959b929a3
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2016-04-12 20:36:06 +0200

    app-service2: use redis as results backend

--- app-service2/run-dev.sh
+++ app-service2/run-dev.sh
@@ -1,10 +1,14 @@
 eval $(docker-machine env)
-RABBIT_ID=$(docker run -d -p 4369:4369 -p 5671:5671 -p 5672:5672 rabbitmq)
+DOCKER_HOST_IP=$(echo $DOCKER_HOST| grep -E -o '\d{1,3}(\.\d{1,3}){3}')
+
 REDIS_ID= $(docker run -d -p 6379:6379 redis:alpine)
+export REDIS_SERVICE_HOST=$DOCKER_HOST_IP
+export REDIS_SERVICE_HOST=6379
 
-
-export RABBITMQ_SERVICE_HOST=$(echo $DOCKER_HOST| grep -E -o '\d{1,3}(\.\d{1,3}){3}')
+RABBIT_ID=$(docker run -d -p 4369:4369 -p 5671:5671 -p 5672:5672 rabbitmq)
+export RABBITMQ_SERVICE_HOST=$DOCKER_HOST_IP
 export RABBITMQ_SERVICE_PORT=5672
+
 export MONGO_WAVER_SERVICE_HOST=127.0.0.1
 export MONGO_WAVER_SERVICE_PORT=27017
 cd src
@@ -19,5 +23,6 @@ CELERY_PID=$!
 python service.py
 docker stop $RABBIT_ID
 docker stop $REDIS_IP
+
 kill $MONGO_PID
 kill $CELERY_PID
--- app-service2/src/task/__init__.py
+++ app-service2/src/task/__init__.py
@@ -1,11 +1,15 @@
 import celery
 import os
 
-broker='amqp://guest@%s:%s/' % (
+broker = 'amqp://guest@%s:%s/' % (
     os.environ['RABBITMQ_SERVICE_HOST'],
     os.environ['RABBITMQ_SERVICE_PORT']
     )
-app = celery.Celery('tasks', broker=broker)
+result_backend = 'redis://%s:%s' % (
+    os.environ['REDIS_SERVICE_HOST'],
+    os.environ['REDIS_SERVICE_PORT']
+    )
+app = celery.Celery('tasks', broker=broker, backend=backend)
 
 from create_app import create_app
 import gcloud
